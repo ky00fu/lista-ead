8b6068885ac27eb319e86434689ed16e1883c0de
ky00fu <kirsycu@gmail.com>
2022-08-26 07:49:24 -0300

    lista ead prontas

--- ex7.c
+++ ex7.c
@@ -4,7 +4,7 @@
 int main(){
 	setlocale(LC_ALL,"");
 	
-<<<<<<< HEAD
+
 	int resposta, res1, res2, res3, res4, res5;
 
 	printf("Você telefonou para vítima?\n");
@@ -12,14 +12,7 @@ int main(){
 	printf("1: SIM\n");
 	printf("0: NÃO\n");
 	scanf("%d",&res1);
-=======
-	/*
-	- telefonou para vitima?
-	- esteve no local do crime?
-	- mora perto da vitima?
-	- devia para a vitima?
-	- ja trabalhou com a vitima?
->>>>>>> e7cb961bbe043c8dbe1a4f3569d6ae31f0392e2e
+
 	
 	if(res1 == 1)
 	{
@@ -32,7 +25,7 @@ int main(){
 	printf("0: NÃO\n");
 	scanf("%d",&res2);
 	
-<<<<<<< HEAD
+
 	if(res2 == 1)
 	{
 		resposta = resposta + 1;
@@ -89,68 +82,5 @@ int main(){
 	{
 		printf("\nVocê é INOCENTE");
 	}
-=======
-	int resposta;
-	
-	printf("Você telefonou para vítima?\n");
-	printf("Digite:\n");
-	printf("1: SIM\n");
-	printf("2: NÃO\n");
-	scanf("%d",&resposta);
-	
-	if(resposta = 1)
-	{
-		resposta = resposta++;
-	}	
-	
-	printf("Esteve no local do crime?\n");
-	printf("Digite:\n");
-	printf("1: SIM\n");
-	printf("2: NÃO\n");
-	scanf("%d",&resposta);	
-	
-	if(resposta = 1)
-	{
-		resposta += resposta;
-	}
-	
-	printf("Mora perto da vítima?\n");
-	printf("Digite:\n");
-	printf("1: SIM\n");
-	printf("2: NÃO\n");
-	scanf("%d",&resposta);
-	
-		if(resposta = 1)
-	{
-		resposta += resposta;
-	}
-
-	
-	printf("Devia para a vítima?\n");
-	printf("Digite:\n");
-	printf("1: SIM\n");
-	printf("2: NÃO\n");
-	scanf("%d",&resposta);
-	
-		if(resposta = 1)
-	{
-		resposta += resposta;
-	}
-
-	
-	printf("Já trabalhou com a vítima?\n");
-	printf("Digite:\n");
-	printf("1: SIM\n");
-	printf("2: NÃO\n");
-	scanf("%d",&resposta);
-
-		if(resposta = 1)
-	{
-		resposta += resposta;
-	}
-	
-	
-
->>>>>>> e7cb961bbe043c8dbe1a4f3569d6ae31f0392e2e
 }
 
